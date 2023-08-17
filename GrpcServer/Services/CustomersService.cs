7d001e1dad4a6d6e1ecd6c6c96ead1a80859bd96
Rohan Sanjay Mahajan <rohansanjay.mahajan@kaplan.edu>
2023-08-17 12:15:25 +0530

    Add project files.

---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,82 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService:Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            this.logger = logger;
+        }
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+           CustomerModel output = new CustomerModel();   
+            
+            if (request.UserId == 1) {
+
+                output.FirstName = "Rohan";
+                output.LastName = "Mahajan";
+            }
+            else if (request.UserId == 2) {
+
+                output.FirstName = "Kusuma";
+                output.LastName = "Kavya";
+
+            }
+            else
+            {
+                output.FirstName = "Varsha";
+                output.LastName = "Pradeep";
+
+            }
+
+            return Task.FromResult(output);
+        }
+        //to add task of new rpc calls
+        public override async Task GetNewCustomers(newCustomerRequest request, 
+            IServerStreamWriter<CustomerModel> responseStream, 
+            ServerCallContext context)
+
+        {
+           
+            List<CustomerModel> customers = new List<CustomerModel>()
+            {
+                new CustomerModel
+                {
+                    FirstName="Vikas",
+                    LastName="Shankar",
+                    EmailID="vikas@gmail.com",
+                    IsALive=true,
+                    Age=50
+                    
+                },
+                new CustomerModel
+                {
+                    FirstName="Rohan",
+                    LastName="Mahajan",
+                    EmailID="rohan@gmail.com",
+                    IsALive=true,
+                    Age=50
+
+                },
+                new CustomerModel
+                {
+                    FirstName="Rahul",
+                    LastName="Raj",
+                    EmailID="rahulR@gmail.com",
+                    IsALive=false,
+                    Age=50
+
+                }
+        };
+
+            foreach(var customer in customers)
+            {
+               await Task.Delay(1000);//adding delay of 1sec for ecah data 
+               await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
---
+++ grpcClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using Grpc.Core;
+
+
+namespace grpcClient
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        
+
+            var channel=GrpcChannel.ForAddress("http://localhost:5242");
+
+            var customerClient=new Customer.CustomerClient(channel);
+
+            var clientRequest = new CustomerLookupModel { UserId = 1 };
+
+            var customer=await customerClient.GetCustomerInfoAsync(clientRequest);
+
+            Console.WriteLine($"{customer.FirstName},{customer.LastName}");
+
+            Console.WriteLine();
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+
+
+            using (var call = customerClient.GetNewCustomers(new newCustomerRequest())) 
+            {
+                while(await call.ResponseStream.MoveNext())//movenext is one inbuit used to import from grpc.core
+
+                {
+                    var currenrtCustomer=call.ResponseStream.Current;
+                    Console.WriteLine($"{currenrtCustomer.FirstName},{currenrtCustomer.LastName}:{currenrtCustomer.EmailID}");
+
+                }
+            }
+                Console.ReadLine();
+        }
+    }
+}
